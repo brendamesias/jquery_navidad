c54206f318d9f6f2b67d366e77096ac3fc38259c
brendamesias <brendamesiasvillar@gmail.com>
2017-12-23 11:26:09 -0500

    nuevos cambios

---
+++ js/singup.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  var $email = $('#inputEmail');
+  var $password = $('#inputPassword');
+
+  // asociando eventos a los inputs
+  $email.on('input', function(){
+    console.log(event.target.value);
+    console.log($(this).val());
+    var PATERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+    console.log(PATERNEMAIL.test($(this).val()));
+  })
+});
