5f099b19b5389227120aecfda91764f3a8820d9f
brendamesias <brendamesiasvillar@gmail.com>
2017-12-23 11:36:34 -0500

    subiendo cambios

--- js/singup.js
+++ js/singup.js
@@ -8,5 +8,13 @@ $(document).ready(function() {
     console.log($(this).val());
     var PATERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
     console.log(PATERNEMAIL.test($(this).val()));
-  })
+  });
+
+  $password.on('input', function(){
+    if ($(this).val().length >=6){
+      alert('cumple');
+    } else {
+      alert('no cumple');
+    }
+  });
 });
